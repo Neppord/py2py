414003c2deb8f328586f1102e9cf187bf61a4688
Samuel Ytterbrink <neppord@gmail.com>
2011-01-21 12:48:25 +0100

    changed name of the compiler script and added a setupscript

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = "py2py",
+    version = "0.1",
+    description = "A Python to Python compiler.",
+    author = "Samuel Ytterbrink",
+    url = "http://github.com/Neppor/py2py",
+    packages = ["py2py_lib"],
+    scripts = ["py2py"]
+    )
