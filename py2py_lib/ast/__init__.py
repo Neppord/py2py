09803d024fb59a1a1223cb738fcd0c556cef06d9
Samuel Ytterbrink <neppord@gmail.com>
2011-01-29 18:34:40 +0100

    moved literal to a new module of the parser

--- py2py_lib/ast/__init__.py
+++ py2py_lib/ast/__init__.py
@@ -1,23 +1,3 @@
-from node import Node.py
-class Literal(Node):
-
-	def __init__(self, string):
-		self.string = string
-		self.clear()
-	
-	def clear(self):
-		self.faild = False
-		self.consume = list(self.string[-1::-1])
-	
-	def feed(self, char):
-		if self.faild:
-			return
-		else:
-			if char == self.consume.pop():
-				if not self.consume:
-					return self.string
-				return
-			else:
-				self.faild = True
-				return
-
+from program import Program
+def parse(string):
+  return Program.parse(string)
---
+++ py2py_lib/ast/literal.py
@@ -0,0 +1,24 @@
+from node import Node.py
+
+class Literal(Node):
+
+	def __init__(self, string):
+		self.string = string
+		self.clear()
+	
+	def clear(self):
+		self.faild = False
+		self.consume = list(self.string[-1::-1])
+	
+	def feed(self, char):
+		if self.faild:
+			return
+		else:
+			if char == self.consume.pop():
+				if not self.consume:
+					return self.string
+				return
+			else:
+				self.faild = True
+				return
+
